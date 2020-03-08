de86df6f212c28bd0b38ca5030b6271f6261f387
wrzonki <wrzonki@gmail.com>
2020-03-08 21:28:25 +0100

    p[oprawka czasu

--- src/tsc/index.ts
+++ src/tsc/index.ts
@@ -24,8 +24,8 @@ const getData = async () => {
 
 const getHours = (time:number) => {
     const dt = new Date(time);
-    const hr = dt.getUTCHours();
-    const m = "0" + dt.getUTCMinutes();
+    const hr = dt.getHours();
+    const m = "0" + dt.getMinutes();
     return hr + ':' + m.substr(-2)
 }
 
@@ -61,7 +61,7 @@ const countdown = t => {
     let distance = t - now;
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) - 1;
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
